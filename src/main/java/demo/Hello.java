e0cd0252fd65b5eb26ebaccab7482f651d1c1b74
freewind <nowindlee@gmail.com>
2018-07-28 22:59:43 +0800

    add the list of string demo

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -9,12 +9,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import static com.github.freewind.lostlist.Lists.list;
+
 public class Hello {
 
     public static void main(String[] args) throws IOException {
-        Page page = new Page(Lists.arrayList(
-                new Item("Item 1", "$19.99", Lists.arrayList(new Feature("New!"), new Feature("Awesome!"))),
-                new Item("Item 2", "$29.99", Lists.arrayList(new Feature("Old."), new Feature("Ugly.")))
+        Page page = new Page(list(
+                new Item("Item 1", "$19.99", list("good", "item", "new"), list(new Feature("New!"), new Feature("Awesome!"))),
+                new Item("Item 2", "$29.99", list("old", "item", "bad"), list(new Feature("Old."), new Feature("Ugly.")))
         ));
         MustacheFactory mf = new DefaultMustacheFactory();
         Mustache mustache = mf.compile(mf.getReader("templates/hello.html"), "hello");
@@ -40,10 +42,12 @@ class Feature {
 class Item {
     final String name;
     final String price;
+    final List<String> keywords;
     final List<Feature> features;
-    public Item(String name, String price, List<Feature> features) {
+    public Item(String name, String price, List<String> keywords, List<Feature> features) {
         this.name = name;
         this.price = price;
+        this.keywords = keywords;
         this.features = features;
     }
 }
